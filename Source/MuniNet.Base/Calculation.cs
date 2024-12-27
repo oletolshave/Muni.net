db324be321293b9b3ea992d167ad2815d9981188
Ole Hyldahl Tolshave <ole@intercompany.io>
2024-12-27 21:40:03 +0100

    Added initial Base project

---
+++ Source/MuniNet.Base/Calculation.cs
@@ -0,0 +1,16 @@
+﻿namespace MuniNet.Base;
+
+public abstract class Calculation
+{
+}
+
+public abstract class Calculation<TOutput, TInput> : Calculation
+{
+    public abstract TOutput Calculate(TInput input);
+}
+
+public abstract class Calculation<TCalc, TOutput, TInput>
+    : Calculation<TOutput, TInput>
+    where TCalc : Calculation<TCalc, TOutput, TInput>
+{
+}
