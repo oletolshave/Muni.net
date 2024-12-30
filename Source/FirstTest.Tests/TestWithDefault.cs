0d0170ce6f6dbb2f59218bdd3460c0bea3d94d16
Ole Hyldahl Tolshave <ole@intercompany.io>
2024-12-30 08:40:10 +0100

    Redis test might now work.

--- Source/FirstTest.Tests/TestWithDefault.cs
+++ Source/FirstTest.Tests/TestWithDefault.cs
@@ -1,28 +1,35 @@
 ﻿using AutoFixture.Xunit2;
 using FluentAssertions;
+using MuniNet.Base;
 using MuniNet.Storage.Memory;
+using MuniNet.StorageAgnostic.Tests;
 using SimpleTestLib;
 
 namespace FirstTest.Tests;
 
 [Trait("Category", "CI")]
-public class TestWithDefaults
+public class TestWithDefaults : AllTests
 {
-    [Theory]
-    [InlineAutoData(0, 0)]
-    [InlineAutoData(1, 1)]
-    [InlineAutoData(6, 8)]
-    [InlineAutoData(8, 21)]
-    public async Task ItReturnsCorrectResultsOnFirstCalculations(int nthNumber, int expectedResult)
+    protected override ICacheManager NewCacheManager()
     {
-        var sut = CacheManagerMemory.New();
-
-        var fibCalc = new FibonacciCalc();
-        var actualResult = await sut.For(fibCalc).CalculateAsync(nthNumber);
-
-        actualResult.Should().Be(expectedResult);
-        fibCalc.CallCount.Should().Be(1);
+        return CacheManagerMemory.New();
     }
+
+    //[Theory]
+    //[InlineAutoData(0, 0)]
+    //[InlineAutoData(1, 1)]
+    //[InlineAutoData(6, 8)]
+    //[InlineAutoData(8, 21)]
+    //public async Task ItReturnsCorrectResultsOnFirstCalculations(int nthNumber, int expectedResult)
+    //{
+    //    var sut = CacheManagerMemory.New();
+
+    //    var fibCalc = new FibonacciCalc();
+    //    var actualResult = await sut.For(fibCalc).CalculateAsync(nthNumber);
+
+    //    actualResult.Should().Be(expectedResult);
+    //    fibCalc.CallCount.Should().Be(1);
+    //}
 
     [Theory]
     [InlineAutoData(0, 0)]
